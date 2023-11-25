f52866520331b8742cbf9a5cce080049b72e262b
LENOVO <LENOVO@DESKTOP-BH2L40U>
2023-11-25 15:13:57 +0200

    add some css

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -6,10 +6,19 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 
 sys.path.append('mysite/myapp/templates/myapp')
+sys.path.append('D:/sql_model/mysite/myapp/static/styles')
 
 class FromForm(forms.Form):
-    email = forms.CharField(label="email")
-    password = forms.CharField(label="password",widget=forms.PasswordInput())
+    email = forms.CharField(widget=forms.TextInput(attrs={
+       'id':'second',
+       'title':'Enter email',
+       'placeholder':'Enter email ...' 
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'id':'first',
+        'title':'Enter password',
+        'placeholder':'Enter password ...'
+        }))
 
 
 
@@ -24,8 +33,7 @@ def mainpage(request):
             pa = form.cleaned_data["password"]
             request.session["pas"] += [pa]
             em_l = form.cleaned_data["email"]
-            request.session["eml"] += em_l
-            return HttpResponseRedirect(reverse("myapp:add"))
+            request.session["eml"] += [em_l]
         else:
             return render(request,'myapp/mainpage.html',{'form':FromForm()})
 
@@ -36,7 +44,7 @@ def add(request):
         request.session["pas"] = []
     if "eml" not in request.session:
         request.session["eml"] = []
-    return render(request,'myapp/add.html',{
-        "pas":request.session["pas"],
-        "eml":request.session["eml"]})
+    
+    zip_pas_eml = zip(request.session["pas"],request.session["eml"])
+    return render(request,'myapp/add.html',{'vars':zip_pas_eml})
 
