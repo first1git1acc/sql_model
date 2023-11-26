635a25e69dccf71dbc0647e8a92a51a9d8acf1d2
LENOVO <LENOVO@DESKTOP-BH2L40U>
2023-11-26 11:19:28 +0200

    add admin site

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+import sys
+
+sys.path.append('D:/sql_model/mysite/myapp')
+
+from myapp.models import Customer,Product
+
+admin.site.register(Product)
+admin.site.register(Customer)
 
 # Register your models here.
